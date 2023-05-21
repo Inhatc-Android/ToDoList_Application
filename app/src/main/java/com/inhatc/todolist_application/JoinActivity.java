4a749cb1af6c7c31219615b326a69cd10bbcdacf
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 <wngns0812@gmail.com>
2023-05-22 01:06:36 +0900

    FEAT: join, login, google-login add function

--- app/src/main/java/com/inhatc/todolist_application/JoinActivity.java
+++ app/src/main/java/com/inhatc/todolist_application/JoinActivity.java
@@ -1,14 +1,140 @@
 package com.inhatc.todolist_application;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class JoinActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText nameText;
+    private EditText emailText;
+    private EditText passwordText;
+    private EditText passwordCheckText;
+    private Button emailCheckButton;
+    private Button cancleButton;
+    private Button joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_join);
+
+        auth = FirebaseAuth.getInstance();
+
+        nameText = (EditText) findViewById(R.id.joinNameText);
+        emailText = (EditText) findViewById(R.id.joinEmailText);
+        passwordText = (EditText) findViewById(R.id.joinPassword);
+        passwordCheckText = (EditText) findViewById(R.id.joinPasswordCheck);
+
+        emailCheckButton = (Button) findViewById(R.id.emailCheckButton);
+        cancleButton = (Button) findViewById(R.id.cancelButton);
+        joinButton = (Button) findViewById(R.id.joinButton);
+
+        emailCheckButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = emailText.getText().toString().trim();
+                if (email.isEmpty()) {
+                    Toast.makeText(JoinActivity.this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    checkEmailExistsInFirebase(email);
+                }
+            }
+        });
+        cancleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        joinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = emailText.getText().toString().trim();
+                String name = nameText.getText().toString().trim();
+                String pw = passwordText.getText().toString().trim();
+                String pwChk = passwordCheckText.getText().toString().trim();
+                if(pw.equals(pwChk)){
+                    auth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                FirebaseUser user = auth.getCurrentUser();
+                                String email = user.getEmail();
+                                String uid = user.getUid();
+
+                                HashMap<Object, String> hashMap = new HashMap<>();
+                                hashMap.put("email", email);
+                                hashMap.put("name", name);
+
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+                                Intent intent = new Intent(JoinActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(JoinActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(JoinActivity.this, "이미 존재하는 아이디입니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(JoinActivity.this, "비밀 번호가 틀렸습니다. 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
+
+    private void checkEmailExistsInFirebase(String email) {
+        FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                        if (task.isSuccessful()) {
+                            SignInMethodQueryResult result = task.getResult();
+                            List<String> signInMethods = result.getSignInMethods();
+                            if (signInMethods != null && !signInMethods.isEmpty()) {
+                                // 이메일이 이미 등록되어 있음
+                                Toast.makeText(JoinActivity.this, "이미 등록된 이메일입니다.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // 이메일이 등록되어 있지 않음
+                                Toast.makeText(JoinActivity.this, "사용 가능한 이메일입니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // 작업 실패
+                            Toast.makeText(JoinActivity.this, "이메일 중복 확인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/inhatc/todolist_application/LoginActivity.java
+++ app/src/main/java/com/inhatc/todolist_application/LoginActivity.java
@@ -1,14 +1,152 @@
 package com.inhatc.todolist_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private GoogleSignInClient googleSignInClient;
+    private static final int code = 9001;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        emailEditText = (EditText) findViewById(R.id.email);
+        passwordEditText = (EditText) findViewById(R.id.password);
+
+        Button signInButton = (Button) findViewById(R.id.emailSignInButton);
+        Button signUpButton = (Button) findViewById(R.id.emailSignUpButton);
+        SignInButton googleLoginButton = findViewById(R.id.googleLoginButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailLogin();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        googleLoginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent, code);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 구글로그인 버튼 응답
+        if (requestCode == code) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // 구글 로그인 성공
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            String userName = acct.getDisplayName();
+                            String userEmail = acct.getEmail();
+                            String uid = auth.getUid();
+
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            hashMap.put("email", userEmail);
+                            hashMap.put("name", userName);
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+                            reference.child(uid).setValue(hashMap);
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(LoginActivity.this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(LoginActivity.this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void emailLogin() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "이메일 혹은 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
